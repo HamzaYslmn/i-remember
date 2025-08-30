5587894a7b2e119c7a09cbe1e57b882b16fea4ad
HamzaYslmn <hamzayesilmen7@gmail.com>
2025-08-30 04:28:14 +0300

    supabase wake, release

--- BackEnd/routes/root.py
+++ BackEnd/routes/root.py
@@ -1,11 +1,17 @@
 from fastapi import APIRouter
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
+from modules.Supabase import xSupabase
 
 router = APIRouter(tags=["Root"])
 
 @router.get("/status")
 async def root():
+    try:
+        await xSupabase.read_sdoc("i-remember", doc_id=["141ed198-7b29-4e32-b64a-f63d5f3b9748"], select="uuid")
+    except Exception as e:
+        print(f"Error: {e}")
+        
     return {
         "status": "online",
         "output": "Welcome to the HamzaYslmn API Service! 🏔️",
